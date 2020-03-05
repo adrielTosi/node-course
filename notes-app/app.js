3ad59c3bf626cdf4ab8f434a92b49f28e36dd8b6
Adriel Tosi <adrielstosi@gmail.com>
2020-03-05 17:36:01 +0100

    Complete Notes App

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,6 @@
 const c = require("chalk");
 const y = require("yargs");
+const notes = require("./notes");
 
 // Add Command
 y.command({
@@ -7,31 +8,56 @@ y.command({
   describe: "Add a new Note",
   builder: {
     title: {
-      describe: "Note title"
+      describe: "Note title",
+      demandOption: true,
+      type: "string"
+    },
+    body: {
+      describe: "Note body",
+      demandOption: true,
+      type: "string"
     }
   },
-  handler: argv => console.log("Add Note done!", argv)
+  handler: ({ body, title }) => {
+    notes.addNotes(title, body);
+  }
 });
 
 // Remove Command
 y.command({
   command: "remove",
   describe: "Remove a note",
-  handler: () => console.log("Remove note!")
+  builder: {
+    title: {
+      describe: "Note Title",
+      demandOption: true,
+      type: "string"
+    }
+  },
+  handler: argv => notes.removeNotes(argv.title)
 });
 
 // List Command
 y.command({
   command: "list",
   describe: "List all notes",
-  handler: () => console.log("List all notes")
+  handler: () => {
+    notes.listNotes();
+  }
 });
 
 // Read Command
 y.command({
   command: "read",
   describe: "read a note",
-  handler: () => console.log("Read notes")
+  builder: {
+    title: {
+      describe: "Note Title",
+      demandOption: true,
+      type: "string"
+    }
+  },
+  handler: argv => notes.readNotes(argv.title)
 });
 
 y.parse();
--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,5 +1,80 @@
+const fs = require("fs");
+const c = require("chalk");
+
 const getNotes = function() {
   return "Your notes...";
 };
 
-module.exports = getNotes;
+const addNotes = (title, body) => {
+  const notes = loadNotes();
+  const isDuplicate = notes.find(note => note.title === title);
+
+  if (!isDuplicate) {
+    notes.push({
+      title,
+      body
+    });
+    saveNotes(notes);
+    console.log("new Note");
+  } else {
+    console.log("No note taken, duplicate title");
+  }
+};
+
+const removeNotes = title => {
+  const notes = loadNotes();
+  const isNote = notes.some(note => note.title === title);
+  if (isNote) {
+    const newNotes = notes.filter(note => note.title !== title);
+    saveNotes(newNotes);
+    console.log(c.green.inverse(`Note with title '${title}' removed.`));
+  } else {
+    console.log(
+      c.red.inverse(`No note with this title, did you really mean '${title}'?`)
+    );
+  }
+};
+
+const listNotes = () => {
+  const notes = loadNotes();
+  notes.forEach(note => {
+    console.log(`-> ${c.bold.bgGray(note.title)}`);
+    console.log(note.body);
+  });
+};
+
+const readNotes = title => {
+  const notes = loadNotes();
+  const noteToRead = notes.find(note => note.title === title);
+
+  debugger;
+
+  if (noteToRead) {
+    console.log(`-> ${c.bold.bgGray(noteToRead.title)}`);
+    console.log(noteToRead.body);
+  } else {
+    console.log(c.red.inverse("No note found!"));
+  }
+};
+
+// Read the notes.json file, read and parse back to JS Object
+const loadNotes = () => {
+  try {
+    return JSON.parse(fs.readFileSync("notes.json").toString());
+  } catch (e) {
+    return [];
+  }
+};
+
+// Receives modified notes, which has a newly added note and rewrites the notes.json file
+const saveNotes = notes => {
+  fs.writeFileSync("notes.json", JSON.stringify(notes));
+};
+
+module.exports = {
+  addNotes,
+  getNotes,
+  removeNotes,
+  listNotes,
+  readNotes
+};
